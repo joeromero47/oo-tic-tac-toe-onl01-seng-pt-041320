48243411d0bad863a2dea2c5be469765b137758d
Joseph Romero <joeromero47@gmail.com>
2020-05-08 23:11:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,20 @@
 class TicTacToe
+  
+  WIN_COMBINATIONS = [ #these are winning combinations
+    [0, 1, 2]
+    [3, 4, 5]
+    [6, 7, 8]
+    [0, 4, 8]
+    [2, 4, 6]
+    [0, 3, 6]
+    [1, 4, 7]
+    [2, 5, 8]
+    ]
 
-
-  def initialize(board = nil)
-    @board
+  def initialize
+    @board = Array.new(9, " ") #starting a new board each time class is instanced
+  end
+  
+  def display_board #drawing a board
+    puts " #{@board[0]} |"
 end
